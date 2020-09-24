d9bc166355f73bd960fddbfb843e6b25bef333ee
jonathan <jonathanduenas94@gmail.com>
2020-09-24 13:21:50 -0600

    feat: landing style

--- src/components/home.js
+++ src/components/home.js
@@ -14,8 +14,8 @@ const Home = ({name, paragraph, authorImage})=>{
           loop
           controls={false}
           muted
-          width="100vw"
-          height="100vh"
+          width="100%"
+          height="100%"
         />
       </div>
     </div>
